c898dc03f008cf3be1131270c68ea24de051683d
linderlagos <linderlagos43@gmail.com>
2018-12-07 09:00:08 -0600

    commint inicial

---
+++ config/yargs.js
@@ -0,0 +1,22 @@
+let opts = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+}
+
+const argv = require('yargs')
+    .command('listar', 'imprime en consola la tabla de multiplicar', opts)
+    .command('crear', 'generea un archivo con la tabla de multiplicar', opts)
+    .help()
+    .argv;
+
+
+
+module.exports = {
+    argv
+}
